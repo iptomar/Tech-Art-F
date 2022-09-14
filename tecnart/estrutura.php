17f90541063aed1fe7537a7ee7c0ec603683e9e4
David Santos <aluno21293@ipt.pt>
2022-09-14 21:36:25 +0100

    Estrutura, Oportunidades criados Sobre Atualizado

---
+++ tecnart/estrutura.php
@@ -0,0 +1,73 @@
+<?php
+include 'models/functions.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+      <?=template_header('Estrutura');?>
+
+
+      <!-- product section -->
+      <section class="product_section layout_padding">
+      <div style="padding-top: 50px; padding-bottom: 30px;">
+         <div class="container">
+            <div class="heading_container2 heading_center2">
+               
+                <h3 style="font-family: 'Merriweather Sans', sans-serif; font-size: 33px; color:#333f50; text-transform: uppercase;">
+                    estrutura orgânica
+                </h3>
+
+                 
+                <h4 style="font-family: 'Quicksand', sans-serif; font-size: 26px; padding-right: 660px; color:#060633; text-transform: uppercase; padding-top: 25px;">
+                    subtítulo
+                </h4>
+
+                <div class="flex-container">
+                    <div class="flex-right">
+                        <b>1. </b>luctus. Maecenas et quam rutrum, dignissim orci sodales, eleifend elit. Praesent viverra odio at volutpat aliquam. <b>Aliquam vitae libero molestie,
+                        laoreet tellus</b> non, pretium orci. Maximus lacus sed, aliquet ex. Integer faucibus ante et magna finibus, ac consequat sapien commodo.<br><br>
+
+                        <b>2. </b>Aenean a condimentum diam. Sed facilisis felis neque, a volutpat libero euismod ut. Donec pellentesque metus at enim tempor fringilla.
+                        Morbi dictum mauris sem, in commodo magna elementum eu. <b>Duis ornare, ex ac rhoncus</b> dictum, magna mauris tincidunt dolor.<br><br>
+
+                        <b>3. </b>Sed odio lacus, bibendum sed leo fringilla, vehicula finibus sem. Fusce urna sem, vehicula a accumsan sed, molestie id mi.
+                        Donec tempus odio nec nisi euismod, ac volutpat purus laoreet. Ut <b>luctus eu libero</b> a fermentum. Sed pretium turpis enim.
+                    </div>
+                    
+                    <div class="flex-left">
+                        <figure class="imgfigura">
+                            <img class="imgestrutura" src="./assets/images/maskingAtHome.jpg" alt="Boat">
+                            <figcaption class="imgs">O encontro, Luís Marques</figcaption>
+                        </figure>
+                    </div>
+                </div>
+
+                <div class="flex-container">
+                    
+                    <div class="flex-left2">
+                        "tempor pulvinar. Vivamus ultrices egestas posuere. Integer magna orci, vestibulum."
+                    </div>
+
+                    <div class="flex-right">
+                        luctus. Maecenas et quam rutrum, dignissim orci sodales, eleifend elit. Praesent viverra odio at volutpat aliquam. Aliquam vitae libero molestie,
+                        laoreet tellus non, pretium orci. Maximus lacus sed, aliquet ex. Integer faucibus ante et magna finibus, ac consequat sapien commodo.
+                        Aenean a condimentum diam. Sed facilisis felis neque, a volutpat libero euismod ut. Donec pellentesque metus at enim tempor fringilla.
+                        Morbi dictum mauris sem, in commodo magna elementum eu. Uis ornare, ex ac rhoncus dictum, magna mauris tincidunt dolor
+                        ed odio lacus, bibendum sed leo fringilla, vehicula finibus sem. Fusce urna sem, vehicula a accumsan sed, molestie id mi.
+                        Donec tempus odio nec nisi euismod, ac volutpat purus laoreet. Luctus eu liber a fermentum. Sed pretium turpis enim.
+                    </div>
+
+                </div>
+               
+
+            </div>
+         </div>
+      </div>
+   </section>
+      <!-- end product section -->
+      
+      <?=template_footer();?>
+
+   </body>
+</html>
