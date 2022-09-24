013b59ed0f38dbc7a0ca38a04cccd30386c55761
Miguel Esteves <miguel-a-c-e@hotmail.com>
2022-09-24 03:13:32 +0100

    botão cancelar corrigido nos projetos

--- projetos/remove.php
+++ projetos/remove.php
@@ -126,7 +126,7 @@
                     </div>
 
                     <div class="form-group">
-                        <button type="submit" onclick="window.location.href = 'index.php'" class="btn btn-danger btn-block">Cancelar</button>
+                        <button type="button" onclick="window.location.href = 'index.php'" class="btn btn-danger btn-block">Cancelar</button>
                     </div>
                 </form>
             </div>
