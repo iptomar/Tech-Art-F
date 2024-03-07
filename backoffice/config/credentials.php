26174f70580dd88185947eaafc02a08a9fe03e2c
pauloagsantos <pauloagsantos@gmail.com>
2024-03-07 14:59:34 +0000

    .

--- backoffice/config/credentials.php
+++ backoffice/config/credentials.php
@@ -2,6 +2,5 @@
 define('USERNAME', 'root');
 define('PASSWORD','');
 define('USERCIENCIA', 'IPT_ADMIN');
-//define('PASSWORDCIENCIA', 'U6-km(jD8a68r'); // testes
-define('PASSWORDCIENCIA', '*******');   // prod
+define('PASSWORDCIENCIA', 'U6-km(jD8a68r'); // testes
 ?>
--- tecnart/config/credentials.php
+++ tecnart/config/credentials.php
@@ -1,6 +1,4 @@
 <?php
-
 define('USERNAME', 'root');
 define('PASSWORD','');
-
 ?>
